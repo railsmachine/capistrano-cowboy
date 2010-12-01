a6f2d8bb736938236d4efe1d112e3944e20ca7f8
Joshua Nichols <josh@technicalpickles.com>
2010-12-01 00:07:21 -0500

    Shuffled files around.

---
+++ lib/capistrano/cowboy.rb
@@ -0,0 +1,29 @@
+require 'capistrano'
+
+module Capistrano
+  class Cowboy
+    def self.load_into(configuration)
+      configuration.load do
+        namespace :cowboy do
+          task :configure do
+            deploy_stage = fetch(:stage, 'none')
+            set :repository, "."
+            set :deploy_via, :copy
+            set :scm, :none
+            set :stage, deploy_stage
+            set :cowboy_deploy, true
+            set :copy_exclude, [".git/*", ".svn/*", "log/*", "vendor/bundle/*"]
+          end
+          desc 'Deploy without SCM'
+          task :default do
+            before 'deploy:update_code', 'cowboy:configure'
+          end
+        end
+      end
+    end
+  end
+end
+
+if Capistrano::Configuration.instance
+  Capistrano::Cowboy.load_into(Capistrano::Configuration.instance)
+end
